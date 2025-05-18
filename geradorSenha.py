a6fbf6db072879ebe248db82803689403a8f47bf
Lucas Siqueira Campo Dall Orto <lucascampo.siqueira@gmail.com>
2025-05-18 20:36:02 -0300

    Configuração de Estilo

--- geradorSenha.py
+++ geradorSenha.py
@@ -9,4 +9,13 @@ ctk.set_default_color_theme("dark-blue")
 app = ctk.CTk()
 app.geometry("500x550")
 app.title("Gerador de Senhas Seguras")
+
+# Configuração de estilo
+TITLE_FONT = ("Roboto", 24, "bold")
+LABEL_FONT = ("Open Sans", 14)
+BUTTON_FONT = ("Open Sans", 14, "bold")
+ENTRY_FONT = ("Open Sans", 13)
+RESULT_FONT = ("Consolas", 18)
+
+
 app.mainloop()

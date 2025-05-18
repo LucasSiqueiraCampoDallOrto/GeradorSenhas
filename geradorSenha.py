cbf2eb48422090725b5a11cd524f8e89c06cc881
Lucas Siqueira Campo Dall Orto <lucascampo.siqueira@gmail.com>
2025-05-18 20:35:25 -0300

    Criando a janela e configurando aparência

--- geradorSenha.py
+++ geradorSenha.py
@@ -1,3 +1,12 @@
 import random
 import customtkinter as ctk
 from tkinter import messagebox
+
+# Configuração de aparência
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
+
+app = ctk.CTk()
+app.geometry("500x550")
+app.title("Gerador de Senhas Seguras")
+app.mainloop()
